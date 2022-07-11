fb129a549c2133bcdeb9da7b10df0f99840cd6c6
Lrapa99 <luisrapalino88@gmail.com>
2022-07-11 13:30:59 -0500

    Hubieron modificaciones

--- ParOImpar.js
+++ ParOImpar.js
@@ -1,18 +1,25 @@
 //* Identificar cual es un numero Par o Impar.
 
-function ParOImpar(valor){
 
-    let cal = valor % 2
+//En este ejemplo exite un objeto que contine la propiedad funciones, la cual tambien posee un objeto como valor, y este a su vez contiene una funcion que realiza tal procedimiento, en este caso es identificar que numero es Par o Impar.
 
-    let resul = valor ? cal == 0 ? `El numero ${valor} es Par`:`El numero ${valor} es Impar` : "No hay datos, primero ingrese un numero"
+const obj = {
+  funciones: {
+    ParOImpar: function ParOImpar(valor) {
+      let cal = valor % 2;
 
-    return resul
-}
+      let resul = valor
+        ? cal == 0
+          ? `El numero ${valor} es Par`
+          : `El numero ${valor} es Impar`
+        : "No hay datos, primero ingrese un numero";
 
-console.log(ParOImpar()); 
-console.log(ParOImpar(2)); 
-console.log(ParOImpar(3)); 
+      return resul;
+    },
+  },
+};
 
 
-
-
+console.log(obj.funciones.ParOImpar());     //!Si no le pasamos un valor
+console.log(obj.funciones.ParOImpar(2));    //*Si le pasamos un numero Par
+console.log(obj.funciones.ParOImpar(15));   //*Si le pasamos un numero Impar
